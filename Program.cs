6363abbf39606bed3dbbf4f4f6af9cd535bb7682
Viktor_Pikaykin <viktor_939@mail.ru>
2023-04-09 13:58:15 +0500

    Task 2

--- Program.cs
+++ Program.cs
@@ -3,4 +3,15 @@
 // Задача 2: Напишите программу, которая на вход принимает два числа
 // и выдаёт, какое число большее, а какое меньшее.
 
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+    Console.WriteLine(num1 + "-max " + num2 + "-min");
+else
+    Console.WriteLine(num1 + "-min " + num2 + "-max");
+*/
 
