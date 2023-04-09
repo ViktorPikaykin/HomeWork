8eac099299d20bdfd9a5981fa107297b9ef3c823
Viktor_Pikaykin <viktor_939@mail.ru>
2023-04-09 14:33:43 +0500

    Task 2

--- Program.cs
+++ Program.cs
@@ -15,3 +15,25 @@ else
     Console.WriteLine(num1 + "-min " + num2 + "-max");
 */
 
+// Задача 4: Напишите программу, которая принимает на вход три числа
+//и выдаёт максимальное из этих чисел.
+
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num2 > max)
+    max = num2;
+if(num3 > max)
+    max = num3;
+Console.WriteLine("number " + max + " maximum ");
+*/
+
+
+
