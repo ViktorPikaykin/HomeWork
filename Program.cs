76328ac5046f01aa8de13c90e81ad14fcf2ff960
Viktor_Pikaykin <viktor_939@mail.ru>
2023-04-09 15:15:33 +0500

    Task 3

--- Program.cs
+++ Program.cs
@@ -35,5 +35,18 @@ if(num3 > max)
 Console.WriteLine("number " + max + " maximum ");
 */
 
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
 
+/*
+Console.Write("Input a namber ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = num % 2;
+
+if(current == 0)
+    Console.WriteLine("Yes!");
+else
+    Console.WriteLine("No!");
+*/
 
